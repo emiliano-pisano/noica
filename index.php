0fe63298ad7e9356dd3bb098bb95004075d93d07
Antonio Pierro <antonio.pierro@gmail.com>
2017-10-10 11:23:51 +0200

    Added fragments for head, main, sidebar and footer

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head><?php require 'pages/fragments/head.php';?></head>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+
+      <header class="main-header"><?php require 'pages/fragments/header.php';?></header>
+
+      <aside class="main-sidebar"><?php require 'pages/fragments/aside.php';?></aside>
+
+      <div class="content-wrapper"><?php require 'pages/fragments/content-wrapper.php';?></div>
+
+      <footer class="main-footer"><?php require 'pages/fragments/footer.php';?></footer>
+
+      <div class="control-sidebar-bg"></div>
+    </div>
+
+    <?php require 'pages/fragments/script.php';?>
+
+  </body>
+</html>
