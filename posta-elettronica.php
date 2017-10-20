b5e1fd826d32c21aa661d9c7098b50405051530d
emiliano-pisano <pisanoemiliano@gmail.com>
2017-10-20 18:04:36 +0200

    Add files via upload

--- posta-elettronica.php
+++ posta-elettronica.php
@@ -147,136 +147,136 @@
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">5 mins ago</td>
+                    <td class="mailbox-date">5 minuti</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">28 mins ago</td>
+                    <td class="mailbox-date">28 minuti</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">11 hours ago</td>
+                    <td class="mailbox-date">11 ore</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">15 hours ago</td>
+                    <td class="mailbox-date">15 ore</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">Yesterday</td>
+                    <td class="mailbox-date">ieri</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">2 days ago</td>
+                    <td class="mailbox-date">2 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">2 days ago</td>
+                    <td class="mailbox-date">2 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">2 days ago</td>
+                    <td class="mailbox-date">2 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">2 days ago</td>
+                    <td class="mailbox-date">2 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">2 days ago</td>
+                    <td class="mailbox-date">2 gionri</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">4 days ago</td>
+                    <td class="mailbox-date">4 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"></td>
-                    <td class="mailbox-date">12 days ago</td>
+                    <td class="mailbox-date">12 gionri</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">12 days ago</td>
+                    <td class="mailbox-date">12 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Assistenza</b> - Temporanea indisponibilità del self service 'Piccolo prestito'...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">14 days ago</td>
+                    <td class="mailbox-date">14 giorni</td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                     <td class="mailbox-name"><a href="leggi-messaggio.php">Emiliano Pisano</a></td>
-                    <td class="mailbox-subject"><b>AdminLTE 2.0 Issue</b> - Trying to find a solution to this problem...
+                    <td class="mailbox-subject"><b>Servizio Previdenza</b> - Segnalazioni di accrediti per importi inferiori a ...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                    <td class="mailbox-date">15 days ago</td>
+                    <td class="mailbox-date">15 giorni</td>
                   </tr>
                   </tbody>
                 </table>
