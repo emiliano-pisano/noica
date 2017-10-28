8700ec7972da3d13084eefb79356f6a4b32d8231
emiliano-pisano <pisanoemiliano@gmail.com>
2017-10-28 20:44:06 +0200

    Update variazioni-dati-utente.php

--- variazioni-dati-utente.php
+++ variazioni-dati-utente.php
@@ -37,36 +37,12 @@
   <footer class="main-footer">
     <?php include 'pages/fragments/footer.php'; ?> 
   </footer>
-
-  
-  <!-- Add the sidebar's background. This div must be placed
+    <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery 3 -->
-<script src="./bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src="./bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="./dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="./dist/js/demo.js"></script>
-<!-- CK Editor -->
-<script src="./bower_components/ckeditor/ckeditor.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="./plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<script>
-  $(function () {
-    // Replace the <textarea id="editor1"> with a CKEditor
-    // instance, using default configuration.
-    CKEDITOR.replace('editor1')
-    //bootstrap WYSIHTML5 - text editor
-    $('.textarea').wysihtml5()
-  })
-</script>
+<?php require 'pages/fragments/script.php';?>
 </body>
 </html>
