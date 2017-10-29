fa2a736758b310b73afc8c3168fb6f146209d1eb
emiliano-pisano <pisanoemiliano@gmail.com>
2017-10-29 17:46:37 +0100

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,43 +21,43 @@ $(function() {
             cmenu_number = -1;
     }
 
-    var menu_number1;
+    var menu_number 1;
     switch(window.location.pathname) {
         
         case '/richiesta-piccolo-prestito.php':
-            menu_number1 = 0;
+            menu_number 1 = 0;
             break;  
         case '/richiesta-anticipazione-ifr-tfr.php':
-            menu_number1 = 0;
+            menu_number 1 = 0;
             break;
         case '/variazione-dati-utente.php':
-            menu_number1 = 0;
+            menu_number 1 = 0;
             break; 
                    
         default:
-            cmenu_number1 = -1;
+            cmenu_number 1 = -1;
     }
 
-    var menu_number2;
+    var menu_number 2;
     switch(window.location.pathname) {
         
         case '/visualizzazione-tfr.php':
-            menu_number2 = 0;
+            menu_number 2 = 0;
             break; 
         case '/visualizzazione-ifr.php':
-            menu_number2 = 0;
+            menu_number 2 = 0;
             break;  
         
             
         default:
-            cmenu_number2 = -1;
+            cmenu_number 2 = -1;
     }
 
-    var menu_number3;
+    var menu_number 3;
     switch(window.location.pathname) {
         
         case '/tool-prestiti.php':
-            menu_number3 = 0;
+            menu_number 3 = 0;
             break;     
             
         default:
