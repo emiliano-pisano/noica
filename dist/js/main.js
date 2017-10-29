79ff810905d7ff0576445973ec7fc0238db09d8b
emiliano-pisano <pisanoemiliano@gmail.com>
2017-10-29 18:06:22 +0100

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,43 +21,43 @@ $(function() {
             cmenu_number = -1;
     }
 
-    var menu_number 1;
+    var menu_number1;
     switch(window.location.pathname) {
         
         case '/richiesta-piccolo-prestito.php':
-            menu_number 1 = 0;
+            menu_number1 = 0;
             break;  
         case '/richiesta-anticipazione-ifr-tfr.php':
-            menu_number 1 = 0;
+            menu_number1 = 0;
             break;
         case '/variazione-dati-utente.php':
-            menu_number 1 = 0;
+            menu_number1 = 0;
             break; 
                    
         default:
-            cmenu_number 1 = -1;
+            cmenu_number1 = -1;
     }
 
-    var menu_number 2;
+    var menu_number2;
     switch(window.location.pathname) {
         
         case '/visualizzazione-tfr.php':
-            menu_number 2 = 0;
+            menu_number2 = 0;
             break; 
         case '/visualizzazione-ifr.php':
-            menu_number 2 = 0;
+            menu_number2 = 0;
             break;  
         
             
         default:
-            cmenu_number 2 = -1;
+            cmenu_number2 = -1;
     }
 
-    var menu_number 3;
+    var menu_number3;
     switch(window.location.pathname) {
         
         case '/tool-prestiti.php':
-            menu_number 3 = 0;
+            menu_number3 = 0;
             break;     
             
         default:
@@ -69,8 +69,8 @@ $(function() {
     });
     $($('.treeview-menu')[menu_number]).parent().toggleClass('menu-open');
     
-    $($('.treeview-menu')[menu_number 1]).css({
+    $($('.treeview-menu')[menu_number1]).css({
         display: 'block'
     });
-    $($('.treeview-menu')[menu_number 1]).parent().toggleClass('menu-open');
+    $($('.treeview-menu')[menu_number1]).parent().toggleClass('menu-open');
 });
