f81ca99b663e1a4467d5f937361abbbcb37cdbec
emiliano-pisano <pisanoemiliano@gmail.com>
2017-10-29 09:05:17 +0100

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,13 @@ $(function() {
         case '/posizione-contributiva.php':
             menu_number = 0;
             break;
+        case '/consultazione-cedolini.php':
+            menu_number = 0;
+            break;
+        case '/consultazione-cu-cud.php':
+            menu_number = 0;
+            break;    
+            
         default:
             cmenu_number = -1;
     }
