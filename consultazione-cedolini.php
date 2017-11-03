7232379bcdbd055b9e1027e4c962688bb66862be
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-04 00:42:46 +0100

    Update consultazione-cedolini.php

--- consultazione-cedolini.php
+++ consultazione-cedolini.php
@@ -23,8 +23,8 @@
         Consultazione cedolini
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Visualizzazione generale</a></li>
+        <li><a href="#"><i class="fa fa-home"></i> Home</a> </li>
+        <li class="active">Visualizzazione generale</li>
         <li class="active"><u>Consultazione cedolini</u></li>
       </ol>
     </section>
