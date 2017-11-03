826f051007f9779084d7f32637772688583a4058
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-04 00:14:49 +0100

    Update content-wrapper.php

--- pages/fragments/content-wrapper.php
+++ pages/fragments/content-wrapper.php
@@ -6,7 +6,7 @@
 </h1>
 <ol class="breadcrumb">
   <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-  <li class="active">Cruscotto</li>
+  <li class="active"><u>Cruscotto</u></li>
 </ol>
 </section>
 
