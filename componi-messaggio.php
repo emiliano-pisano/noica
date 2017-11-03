51c22770ff09e27f2fd6602e9120f54e33e7b715
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-03 23:45:14 +0100

    Update componi-messaggio.php

--- componi-messaggio.php
+++ componi-messaggio.php
@@ -25,7 +25,8 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Posta elettronica</li>
+       <li><a href="#">Posta elettronica</a></li>
+        <li class="active"><u>Componi messaggio</u></li>
       </ol>
     </section>
 
