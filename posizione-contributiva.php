d826bb29ba1310683cd22a2dc7e68e48dd52f5d5
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-04 00:49:04 +0100

    Update posizione-contributiva.php

--- posizione-contributiva.php
+++ posizione-contributiva.php
@@ -23,8 +23,8 @@
         Posizione contributiva
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Visualizzazione generale</a></li>
+        <li><a href="index.php"><i class="fa fa-home"></i> Home</a> </li>
+        <li class="active">Visualizzazione generale</li>
         <li class="active"><u>Posizione contributiva</u></li>
       </ol>
     </section>
