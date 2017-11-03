0dcaaf36bf2edda549c08d6fe88403fc493ccac0
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-04 00:54:45 +0100

    Update visualizzazione-tfr.php

--- visualizzazione-tfr.php
+++ visualizzazione-tfr.php
@@ -23,8 +23,8 @@
         Visualizzazione TFR
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Gestione fine rapporto</a></li>
+        <li><a href="index.php"><i class="fa fa-home"></i> Home</a> </li>
+        <li class="active">Gestione fine rapporto</li>
         <li class="active"><u>Visualizzazione TFR</u></li>
       </ol>
     </section>
