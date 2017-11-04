3fce1582a1e89f5e8478432366caae5a123b6c49
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-04 01:01:35 +0100

    Update componi-messaggio.php

--- componi-messaggio.php
+++ componi-messaggio.php
@@ -48,7 +48,7 @@
             <div class="box-body no-padding">
               <ul class="nav nav-pills nav-stacked">
                 <li><a href="posta-elettronica.php"><i class="fa fa-inbox"></i> Posta in arrivo
-                  <span class="label label-primary pull-right">12</span></a></li>
+                  <span class="label label-primary pull-right">13</span></a></li>
                 <li><a href="#"><i class="fa fa-envelope-o"></i> Posta inviata</a></li>
                 <li><a href="#"><i class="fa fa-file-text-o"></i> Bozze</a></li>
                 <li><a href="#"><i class="fa fa-filter"></i> Posta indesiderata <span class="label label-warning pull-right">65</span></a>
