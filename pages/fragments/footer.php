1343a36e664115862af7a64a93f35a43a8ca07e6
emiliano-pisano <pisanoemiliano@gmail.com>
2017-11-10 00:49:56 +0100

    Update footer.php

--- pages/fragments/footer.php
+++ pages/fragments/footer.php
@@ -1,4 +1,4 @@
 <div class="pull-right hidden-xs">
   <b>Versione</b> 2.4.0
 </div>
-<strong>Copyright &copy; 2017 <a href="https://regione.sardegna.it">Regione Autonoma della Sardegna</a>.</strong> Tutti i diritti riservati.
+<strong>Copyright &copy; 2017 <a href="regione.php"><span>Regione Autonoma della Sardegna</span></a>.</strong> Tutti i diritti riservati.
